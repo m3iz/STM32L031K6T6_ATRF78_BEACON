bf28272c204965683d7abccb3df68d3c0b077f5b
Nikita Saveliev <nsaveliev@heatbit.com>
2026-03-24 11:56:02 +0300

    last tasted but not approved

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -30,10 +30,10 @@
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 
-#define RSSI_THRESHOLD       6    // порог чувствительности
+#define RSSI_THRESHOLD       4    // порог чувствительности
 #define RSSI_ON_COUNT        25      // сколько подтверждений нужно для включения //25
 #define RSSI_OFF_COUNT       30      // сколько подтверждений нужно для выключения
-#define RSSI_THRESHOLD_LOW 	 3
+#define RSSI_THRESHOLD_LOW 	 1
 
 /* USER CODE END PD */
 
